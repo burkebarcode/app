ed4a775891380c1c057702e1c91499f1a2318937
Burke Butler <burke.butler@rackdog.com>
2025-11-18 17:34:31 -0500

    Initial Commit

---
+++ barcode/barcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodeApp.swift
+//  barcode
+//
+//  Created by Burke Butler on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct barcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
